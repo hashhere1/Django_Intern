8839d3d90907de7d2a73ea2437cd2af28f9704b4
hashhere1 <aleemhassaan70@gmail.com>
2025-08-08 19:24:29 +0500

    Added the admin functions

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -9,11 +9,17 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(1)])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["title"]
 
 class Customer(models.Model):
 
@@ -31,6 +37,12 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null= True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    class Meta:
+        ordering = ["first_name", "last_name"]
     
 
 class Order(models.Model):
@@ -48,6 +60,9 @@ class Order(models.Model):
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    
+        
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
@@ -63,6 +78,12 @@ class Address(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name="+")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["title"]
 
 
 class Cart(models.Model):
